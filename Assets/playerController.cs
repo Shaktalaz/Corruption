b30c0d7869cb86a6ead4f9d99281a009ce51b2e6
Shaktalaz <shaktalaz@live.de>
2018-10-24 13:03:23 +0200

    start

---
+++ Assets/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    Vector3 moveDirection;
+    CharacterController characterController;
+	// Use this for initialization
+	void Start () {
+        characterController = gameObject.GetComponent<CharacterController>();
+        moveDirection = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal"));
+
+
+	}
+}
